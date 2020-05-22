e7bea9435537cbd7092c30bc2375b6311252daf0
kramarkirill <kkramar@kantiana.ru>
2020-05-22 09:02:10 +0200

    Initial Commit

---
+++ Lesson2Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson2Task
+//
+//  Created by Кирилл Крамар on 22.05.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
